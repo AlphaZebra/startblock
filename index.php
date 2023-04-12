33ee991eb41641f6aa47f8f4e06aafab9b1ec295
Robert Richardson <robert@peakzebra.com>
2023-04-12 08:50:14 -0400

    "stable version of subscribe, but without cookie"

--- index.php
+++ index.php
@@ -22,22 +22,21 @@ define('PZ_PLUGIN_DIR', plugin_dir_path(__FILE__));
  add_action('init', 'pz_register');
 
 
- 
-
-
-
  class PZQuestions {
     function __construct() {
         global $wpdb;
 
         $this->charset = $wpdb->get_charset_collate(); 
         $this->question_tablename = $wpdb->prefix . "pz_question";
+        $this->startblock_tablename = $wpdb->prefix . "pz_startblock";
+        $this->answers_tablename = $wpdb->prefix . "pz_answers";
     
         add_action('activate_startblock/index.php', array($this, 'onActivate'));
     
         add_action('admin_post_do-segment', array($this, 'do_segment'));
         add_action('admin_post_nopriv_do-segment', array($this, 'do_segment'));
-        //add_action( 'post_updated', array($this, 'pz_on_save'));
+        add_action('admin_post_do-startblock', array($this, 'do_startblock'));
+        add_action('admin_post_nopriv_do-startblock', array($this, 'do_startblock'));
         
  
     }    
@@ -48,45 +47,67 @@ define('PZ_PLUGIN_DIR', plugin_dir_path(__FILE__));
         require_once( ABSPATH . 'wp-admin/includes/upgrade.php');
     
         dbDelta("CREATE TABLE $this->question_tablename (
-            personID bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+            personID bigint(20) unsigned NOT NULL DEFAULT 1,
             qslug varchar(30) NOT NULL DEFAULT '',
             qchoice varchar(60) NOT NULL DEFAULT '',
             PRIMARY KEY  (personID)
         ) $this->charset;");
         
-        } 
-
-        // function pz_on_save($postID, $post_before, $post_after) {
+        dbDelta("CREATE TABLE $this->startblock_tablename (
+            personID bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+            fname varchar(60) NOT NULL DEFAULT '',
+            lname varchar(60) NOT NULL DEFAULT '',
+            email varchar(100) NOT NULL DEFAULT '',
+            created varchar(12) NOT NULL DEFAULT '',
+            PRIMARY KEY  (personID)
+        ) $this->charset;");
         
-        // }
+       
+    } 
+
     
 
     function do_segment () {
         global $wpdb;
-        // var_dump($_POST['question']);
-        // exit;
-
-        // if called from admin side, then we need to save or update values from the block
-        if( is_admin()) {
-            // for each question in the question array
-            //    write record for the question text
-            //    for each answer in the question's answer array
-            //      write answer record
-            // $item = [];
-            // $item['qnum'] = 1;
-            // $item['qtext'] = sanitize_text_field($_POST['question']);
-            // $item['qtype'] = 'A';
         
-            // if( $wpdb->insert($this->question_tablename, $item ) <= 0 ) {  
-            //     var_dump( $wpdb );
-            //     exit;
+       
+            
+        $item = [];
+        $item['personID'] = 1;
+        $item['qslug'] = sanitize_text_field($_POST['qslug']);
+        $item['qchoice'] = 'A';
+        
+        if( $wpdb->insert($this->question_tablename, $item ) <= 0 ) {  
+            var_dump( $wpdb );
+            exit;
 
-            // }
         }
+        
         wp_redirect('/');
         exit;
     }
+    
+    function do_startblock () {
+        global $wpdb;
+        $created = date("m/j/Y");
+            
+            $item = [];
+            $item['personID'] = null;
+            $item['fname'] = sanitize_text_field($_POST['fname']);
+            $item['lname'] = sanitize_text_field($_POST['lname']);
+            $item['email'] = sanitize_text_field($_POST['email']);
+            $item['created']= $created;
+        
+            if( $wpdb->insert($this->startblock_tablename, $item ) <= 0 ) {  
+                var_dump( $wpdb );
+                exit;
+
+            }
+        
+        wp_redirect('/');
+        exit;
 
     }
+}
  
     $pzQuestions = new PZQuestions();
